04560c7bfccd3ff7741514896c4a72f84ff86e1a
srishtisinghhh <88935165+srishtisinghhh@users.noreply.github.com>
2025-12-18 22:38:49 +0530

    Create Province.sql

---
+++ Data Insertion/Province.sql
@@ -0,0 +1,7 @@
+USE OnlineShopping;
+
+INSERT INTO Province VALUES ('Maharashtra');
+INSERT INTO Province VALUES ('Karnataka');
+INSERT INTO Province VALUES ('Tamil Nadu');
+INSERT INTO Province VALUES ('Uttar Pradesh');
+INSERT INTO Province VALUES ('West Bengal');
