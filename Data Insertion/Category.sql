1b7c555451c97014b99762aa7a274eb2dd08a762
srishtisinghhh <88935165+srishtisinghhh@users.noreply.github.com>
2026-01-05 22:08:35 +0530

    Update Category.sql

--- Data Insertion/Category.sql
+++ Data Insertion/Category.sql
@@ -1,12 +1,13 @@
-use OnlineShopping;
-
-insert into Category values('Android Smart TV Box/Air Mouse')
-insert into category values('Attendance Machine')
-insert into category values('Barcode Scanner/Thermal Receipt Printer')
-insert into category values('Bluetooth Handsfree')
-insert into category values('Bluetooth Headphone')
-insert into category values('Bluetooth Speakers')
-insert into category values('Power Bank')
-insert into category values('Security Cameras and Gadgets')
-insert into category values('Smart Watch')
-insert into category values('USB Hub')
+insert into Category 
+(CategoryName)
+values
+('Android Smart TV Box/Air Mouse'),
+('Attendance Machine'),
+('Barcode Scanner/Thermal Receipt Printer'),
+('Bluetooth Handsfree'),
+('Bluetooth Headphone'),
+('Bluetooth Speakers'),
+('Power Bank'),
+('Security Cameras and Gadgets'),
+('Smart Watch'),
+('USB Hub');
