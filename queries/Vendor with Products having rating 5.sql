04c6d161d34bcf55dbbba8fd6fe51da52140c4b9
srishtisinghhh <88935165+srishtisinghhh@users.noreply.github.com>
2026-01-06 15:11:41 +0530

    Create Vendor with Products having rating 5.sql

---
+++ queries/Vendor with Products having rating 5.sql
@@ -0,0 +1,28 @@
+
+Select Name as VendorName, ProductName, Rating
+From Vendor
+	
+	inner join (
+		Select ProductName, VendorID, Rating
+		From Product
+
+			inner join (
+				Select ProductID, VendorID, Rating
+				From VendorProduct
+
+					inner join (
+						Select VendorProductID, Rating
+						From OrderedProduct
+
+							inner join (
+								Select OrderedProductID, Rating
+								From Review Where Rating = 5
+							) opid on opid.OrderedProductID = OrderedProduct.OrderedProductID
+
+					) vpid on vpid.VendorProductID = VendorProduct.VendorProductID
+
+			) pid on pid.ProductID = Product.ProductID
+
+	) vid on vid.VendorID = Vendor.VendorID
+
+Order By VendorName, ProductName
